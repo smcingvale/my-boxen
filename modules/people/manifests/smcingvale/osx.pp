6d99b581bbc7fdf6b39397cb96517433b330583a
Sam McIngvale <smcingvale@ml-smcingvale1-2.local>
2015-05-31 15:49:08 -0500

    Add docs comment

--- modules/people/manifests/smcingvale/osx.pp
+++ modules/people/manifests/smcingvale/osx.pp
@@ -1,6 +1,7 @@
 # == Description
 #
 # OSX settings specific for Sam McIngvale (aka "smcingvale").
+# See https://github.com/boxen/puppet-osx for docs
 #
 class people::smcingvale::osx {
   include osx::keyboard::capslock_to_control
