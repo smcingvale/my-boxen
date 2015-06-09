0890b6bd22e42dd56a9f955f32acb181d59bcbb0
Sam McIngvale <smcingvale@ml-smcingvale1-2.local>
2015-06-09 17:25:41 -0500

    Install brewcask

--- modules/people/manifests/smcingvale.pp
+++ modules/people/manifests/smcingvale.pp
@@ -3,6 +3,7 @@
 # Machine configuration specific for Sam McIngvale (aka "smcingvale").
 #
 class people::smcingvale {
+  include people::smcingvale::apps
   include people::smcingvale::ruby
   include people::smcingvale::osx
 }
---
+++ modules/people/manifests/smcingvale/apps.pp
@@ -0,0 +1,10 @@
+# == Description
+#
+# Apps specific for Sam McIngvale (aka "smcingvale").
+# Use brewcask to manage apps.
+# See https://github.com/boxen/puppet-brewcask and
+# https://github.com/caskroom/homebrew-cask for docs.
+#
+class people::smcingvale::apps {
+  include brewcask
+}
