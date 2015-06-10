0aac8fa4fe38c0829e2091ff69bda8ed2e914cfe
Sam McIngvale <smcingvale@ml-smcingvale1-2.local>
2015-06-10 08:03:51 -0700

    Comment

--- modules/people/manifests/smcingvale/dotfiles.pp
+++ modules/people/manifests/smcingvale/dotfiles.pp
@@ -2,7 +2,7 @@
 #
 # Dotfiles specific for Sam McIngvale (aka "smcingvale").
 #
-# Agruable, the dotfiles repo is a project and should be configured
+# Agruably, the dotfiles repo is a project and should be configured
 # via a /project module.  However, dotfiles are, IMO, more 
 # "machine-configuration" rather than an actively developed
 # project.  Therefore, I configure them here.
