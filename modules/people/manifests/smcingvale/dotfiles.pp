4e3006bcb0259c174be0f49663a5ff1b10ebf099
Sam McIngvale <smcingvale@icloud.com>
2015-06-14 19:59:54 -0500

    Set-up vim

--- modules/people/manifests/smcingvale/dotfiles.pp
+++ modules/people/manifests/smcingvale/dotfiles.pp
@@ -38,4 +38,72 @@ class people::smcingvale::dotfiles {
     target  => "${dotfiles_dir}/vim/gvimrc.symlink",
     require => Repository[$dotfiles_dir],
   }
+
+  # setup .vim and vim plugins
+  $vim = "${home}/.vim"
+  file { [
+    "${vim}",
+    "${vim}/bundle",
+    "${vim}/autoload",
+  ]:
+    ensure => directory,
+  }
+
+  # puppet doesn't like grabbing files like this...
+  exec { 'retrieve_pathogen.vim':
+    command => "/usr/bin/curl -LSso ~/.vim/autoload/pathogen.vim https://tpo.pe/pathogen.vim",
+    creates => "${vim}/autoload/pathogen.vim",
+  }
+
+  file { "${vim}/autoload/pathogen.vim":
+    mode    => 0755,
+    require => Exec["retrieve_pathogen.vim"],
+  }
+
+  # vim plugins
+  $vim_bundle = "${vim}/bundle"
+  repository { "${vim_bundle}/vim-colors-solarized":
+    ensure => 'origin/HEAD',
+    source => 'altercation/vim-colors-solarized',
+  }
+
+  repository { "${vim_bundle}/vim-sensible":
+    ensure => 'origin/HEAD',
+    source => 'tpope/vim-sensible',
+  }
+
+  repository { "${vim_bundle}/vim-sleuth":
+    ensure => 'origin/HEAD',
+    source => 'tpope/vim-sleuth',
+  }
+
+  repository { "${vim_bundle}/vim-fugitive":
+    ensure => 'origin/HEAD',
+    source => 'tpope/vim-fugitive',
+  }
+
+  repository { "${vim_bundle}/vim-surround":
+    ensure => 'origin/HEAD',
+    source => 'tpope/vim-surround',
+  }
+
+  repository { "${vim_bundle}/vim-commentary":
+    ensure => 'origin/HEAD',
+    source => 'tpope/vim-commentary',
+  }
+
+  repository { "${vim_bundle}/vim-tbone":
+    ensure => 'origin/HEAD',
+    source => 'tpope/vim-tbone',
+  }
+
+  repository { "${vim_bundle}/vim-eunuch":
+    ensure => 'origin/HEAD',
+    source => 'tpope/vim-eunuch',
+  }
+
+  repository { "${vim_bundle}/vim-rbenv":
+    ensure => 'origin/HEAD',
+    source => 'tpope/vim-rbenv',
+  }
 }
