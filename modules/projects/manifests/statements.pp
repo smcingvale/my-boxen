c37cb7bb2e19470c4ee97250b1f25b5260f875e8
Sam McIngvale <smcingvale@icloud.com>
2015-06-30 18:54:55 -0500

    Install statements repo

---
+++ modules/projects/manifests/statements.pp
@@ -0,0 +1,14 @@
+class projects::statements {
+  include git
+  $statements_dir = "${::boxen_srcdir}/smcingvale/statements"
+
+  repository { $statements_dir :
+    source => 'http://github.apexclearing.local/apex-public/atlas.git',
+  }
+
+  git::config::local { $statements_dir :
+    key     => 'user.email',
+    value   => 'smcingvale@apexclearing.com',
+    require => Repository[$statements_dir],
+  }
+}
